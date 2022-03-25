1c004940bf753e17a4e977d04b4d25d9baa7a800
Roman Ojha <razzroman98@gmail.com>
2022-03-25 18:53:09 +0545

    class initilizing node/express application

---
+++ Class/server/Topics_wise/05_Express_App_Setup/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});

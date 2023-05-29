c3bbc6fe7377f813c19c8b216fda69057f60e7f7
Roman-Ojha <razzroman98@gmail.com>
2023-05-29 12:43:41 +0545

    feat(practice): created simple User & Post object types

--- Practice/01_Prisma_and_GraphQL_with_Apollo/index.ts
+++ Practice/01_Prisma_and_GraphQL_with_Apollo/index.ts
@@ -1,8 +1,27 @@
 import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
 import { readFileSync } from "fs";
 
 const typeDefs = readFileSync("./schema/schema.graphql", { encoding: "utf-8" });
 
+const app = express();
+const httpServer = http.createServer(app);
+
+// Set up Apollo Server
 const server = new ApolloServer({
   typeDefs,
+  // resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+await server.start();
+
+app.use(cors(), bodyParser.json(), expressMiddleware(server));
+
+httpServer.listen({ port: 8080 }, () => {
+  console.log(`🚀 Server ready at http://localhost:8080`);
 });
--- graphql.config.js
+++ graphql.config.js
@@ -1,23 +1,25 @@
 module.exports = {
   projects: {
     app: {
-      schema: ["Tutorial/01_Object_Types/interface.graphql"],
-      documents: ["Tutorial/01_Object_Types/interface.graphql"],
+      schema: ["./Practice/01_Prisma_and_GraphQL_with_Apollo/schema/*.graphql"],
+      documents: [
+        "./Practice/01_Prisma_and_GraphQL_with_Apollo/schema/*.graphql",
+      ],
     },
-    // db: {
-    //   schema: "src/generated/db.graphql",
-    //   documents: ["src/db/**/*.graphql", "my/fragments.graphql"],
-    //   extensions: {
-    //     codegen: [
-    //       {
-    //         generator: "graphql-binding",
-    //         language: "typescript",
-    //         output: {
-    //           binding: "src/generated/db.ts",
-    //         },
-    //       },
-    //     ],
-    //   },
-    // },
+    db: {
+      schema: "src/generated/db.graphql",
+      documents: ["src/db/**/*.graphql", "my/fragments.graphql"],
+      extensions: {
+        codegen: [
+          {
+            generator: "graphql-binding",
+            language: "typescript",
+            output: {
+              binding: "src/generated/db.ts",
+            },
+          },
+        ],
+      },
+    },
   },
 };
